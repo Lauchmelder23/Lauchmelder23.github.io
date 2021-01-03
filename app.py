47e9c95388393fbf0edaadc24dddb76608c800c6
Robert <robert.trololo@gmail.com>
2021-01-03 01:39:12 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('index.html', svg="")
+
+@app.route('/submit_kanji', methods=['GET'])
+def submit_kanji():
+    with open(f"assets/0{format(ord(request.args.get('kanji')), 'x')}.svg", encoding='utf-8') as file:
+        return render_template('index.html', svg=file.read())
